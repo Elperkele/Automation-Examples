5331a4e496c96c5bfbb412b6f487ed0838d4c46d
konrad <konrad.czyz@o2.pl>
2024-01-31 17:39:21 +0100

    Login test improvements

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,13 +5,34 @@ test(`Verify Login page ${config.name} version`, async ({ login, page }) => {
     await test.step(`Navigate to Login ${config.name} version`, async () => {
         await login.navigateToLogin();
     });
-    await test.step (`Input credentials ${config.name} version`, async () => {
+    await test.step (`Click login button when credentials are empty ${config.name} version`, async () => {
+        await login.clickLogin();
+    });
+    await test.step (`Check error popups for empty credentials ${config.name} version`, async () => {
+        await login.checkErrorPopupEmpty();
+    });
+    await test.step (`Input credentials with incorrect email format${config.name} version`, async () => {
         await login.inputCredentialsWrongFormat();
     });
     await test.step (`Click login button ${config.name} version`, async () => {
         await login.clickLogin();
     });
     await test.step (`Check login format error message ${config.name} version`, async () => {
-        await login.checkErrorPopup();
+        await login.checkErrorPopupFormat();
+    });
+    await test.step(`Input wrong credentials in correct format ${config.name} version`, async () => {
+        await login.inputCredentialsInvalid();
+    });
+    await test.step (`Click login button ${config.name} version`, async () => {
+        await login.clickLogin();
+    });
+    await test.step(`Input correct email or password popup ${config.name} version`, async () => {
+        await login.inputCredentialsValid();
+    });
+    await test.step (`Click login button ${config.name} version`, async () => {
+        await login.clickLogin();
+    });
+    await test.step (`Verify if the user is logged in and redirected to the account page ${config.name} version`, async () => {
+        await login.checkPostLoginURL();
     });
 });
